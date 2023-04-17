aac32e734208292ddd64a59a4c36e5002100d58b
Ashwin-Dev-P <ashwindev1462001@gmail.com>
2023-04-17 15:28:11 +0530

    Identity Authentication

---
+++ Areas/Identity/Data/TextEditorContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using TextEditor.Areas.Identity.Data;
+
+namespace TextEditor.Data;
+
+public class TextEditorContext : IdentityDbContext<TextEditorUser>
+{
+    public TextEditorContext(DbContextOptions<TextEditorContext> options)
+        : base(options)
+    {
+    }
+
+    protected override void OnModelCreating(ModelBuilder builder)
+    {
+        base.OnModelCreating(builder);
+        // Customize the ASP.NET Identity model and override the defaults if needed.
+        // For example, you can rename the ASP.NET Identity table names and more.
+        // Add your customizations after calling base.OnModelCreating(builder);
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,55 @@
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+using TextEditor.Areas.Identity.Data;
+using TextEditor.Data;
+namespace TextEditor
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var builder = WebApplication.CreateBuilder(args);
+            var connectionString = builder.Configuration.GetConnectionString("TextEditorContextConnection") ?? throw new InvalidOperationException("Connection string 'TextEditorContextConnection' not found.");
+
+            builder.Services.AddDbContext<TextEditorContext>(options =>
+                options.UseSqlServer(connectionString)
+            );
+
+            builder.Services.AddDefaultIdentity<TextEditorUser>(options => 
+                options.SignIn.RequireConfirmedAccount = true)
+                .AddEntityFrameworkStores<TextEditorContext>();
+
+            // Add services to the container.
+            builder.Services.AddControllersWithViews();
+            builder.Services.AddRazorPages();
+
+            var app = builder.Build();
+
+            // Configure the HTTP request pipeline.
+            if (!app.Environment.IsDevelopment())
+            {
+                app.UseExceptionHandler("/Home/Error");
+            }
+            app.UseStaticFiles();
+
+            app.UseRouting();
+            app.UseAuthentication();;
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapControllerRoute(
+                    name: "default",
+                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
+            });
+
+            app.MapControllerRoute(
+                name: "default",
+                pattern: "{controller=Home}/{action=Index}/{id?}");
+
+            app.Run();
+        }
+    }
+}
